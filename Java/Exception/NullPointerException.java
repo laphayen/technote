783e627b38f5d8ba9fc4b305be99dfc628925e5d
Baek Gi Chan <laphayen@gmail.com>
2024-01-19 20:10:05 +0900

    Delete NullPointerException.java

--- Java/Exception/NullPointerException.java
+++
@@ -1,8 +0,0 @@
-package test;
-
-public class Example {
-    public static void main(String[] args) {
-        String str = null;
-        int length = str.length(); // NullPointerException
-    }
-}

5652c1ee36fb7c452047e26f6402bc5d77edc143
Baek Gi Chan <laphayen@gmail.com>
2024-01-19 20:14:13 +0900

    Upload case3.java

---
+++ Java/Exception/NullPointerException/case3.java
@@ -0,0 +1,12 @@
+package test;
+
+class Obj {
+    String text;
+}
+
+public class Example {
+    public static void main(String[] args) {
+        Obj obj = new Obj();
+        int length = obj.text.length();
+    }
+}

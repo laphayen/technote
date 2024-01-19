a9d9b588ce35ab827bc7776071d201e78d4e9a3f
Baek Gi Chan <laphayen@gmail.com>
2024-01-19 19:28:06 +0900

    Upload NullPointerException.java

---
+++ Java/Exception/NullPointerException.java
@@ -0,0 +1,8 @@
+package test;
+
+public class Example {
+    public static void main(String[] args) {
+        String str = null;
+        int length = str.length(); // NullPointerException
+    }
+}

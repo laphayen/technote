e9e1c331ba1abc367055b1e0a21d4fc2b23c2512
Baek Gi Chan <laphayen@gmail.com>
2024-01-21 16:37:43 +0900

    Upload Array.java

---
+++ Java/DataType/ReferenceType/Array/Array.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Example {
+    public static void main(String[] args) {
+    	int[] numbers1;
+    	int numbers2[];
+    	
+    	String[] names1 = new String[] {"hello", "codeomni", "tistory"};
+    	String[] names2 = {"good", "java"};
+    }
+}
